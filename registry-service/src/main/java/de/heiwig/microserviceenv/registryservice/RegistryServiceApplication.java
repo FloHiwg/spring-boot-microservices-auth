c699c982ba5ebf09c37371f5ac9b36da94fc7e40
Florian Heiwig <florian.hiwg@gmail.com>
2019-05-17 22:23:39 +0200

    init commit with three modules: gateway service, config service and registry service

---
+++ registry-service/src/main/java/de/heiwig/microserviceenv/registryservice/RegistryServiceApplication.java
@@ -0,0 +1,15 @@
+package de.heiwig.microserviceenv.registryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServiceApplication.class, args);
+	}
+
+}
